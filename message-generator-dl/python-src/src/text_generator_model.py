683d8365c18ec005e21b5305b1b1104f352626c2
Mnemotechnician <daytimer253@gmail.com>
2023-05-21 18:00:21 +0300

    Use dropout during training

--- message-generator-dl/python-src/src/text_generator_model.py
+++ message-generator-dl/python-src/src/text_generator_model.py
@@ -9,6 +9,7 @@ class TextGeneratorModel(tf.keras.Model):
             return_sequences=True,
             return_state=True
         )
+        self.dropout = tf.keras.layers.Dropout(0.1)
         self.gru2 = tf.keras.layers.GRU(
             rnn_units // 4,
             return_sequences=True,
@@ -23,16 +24,22 @@ class TextGeneratorModel(tf.keras.Model):
         x = inputs
         x = self.embedding(x, training=training)
 
+        # GRU 1
         if states is None:
             states = (self.gru1.get_initial_state(x), None)
         x, newState = self.gru1(x, initial_state=states[0], training=training)
         states = (newState, states[1])
 
+        # Dropout
+        x = self.dropout(x, training=training)
+
+        # GRU 2
         if states[1] is None:
             states = (states[0], self.gru2.get_initial_state(x))
         x, newState = self.gru2(x, initial_state=states[1], training=training)
         states = (states[0], newState)
 
+        # Output
         x = self.dense(x, training=training)
 
         if return_state:
